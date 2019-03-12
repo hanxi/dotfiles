d61eb6e0e27b9960a1ca20b1fb7482a97df13c93
涵曦 <hanxi.info@gmail.com>
2019-03-12 19:57:09 +0800

    history and git tree

--- etc/config.sh
+++ etc/config.sh
@@ -6,14 +6,15 @@ fi
 
 # enable bash completion
 # yum install bash-completion
-[[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] && \
-    . /usr/share/bash-completion/bash_completion
+[[ $PS1 && -f /usr/local/share/bash-completion/bash_completion ]] && \
+    . /usr/local/share/bash-completion/bash_completion
 
 # git completion
 # curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.local/etc/git-completion.bash
 if [ -f "$HOME/.local/etc/git-completion.bash" ]; then
     . $HOME/.local/etc/git-completion.bash
 fi
+
 
 # for .local lib
 export LD_RUN_PATH=$HOME/.local/lib::$LD_RUN_PATH
@@ -25,13 +26,17 @@ export TERM=screen-256color
 export TERM_ITALICS=true
 
 # for http proxy
-#export http_proxy=http://192.168.16.12:1080/
+#export http_proxy=http://192.168.29.221:5719/
 export http_proxy=
 export https_proxy=$http_proxy
 export ftp_proxy=$http_proxy
 export rsync_proxy=$http_proxy
 
 # alias
-# alias vim=nvim
+alias vim=nvim
 alias svn=svn-color.py
 
+export HISTTIMEFORMAT="%d/%m/%y %T "
+
+# Alias for tree view of commit history.
+git config --global alias.tree "log --all --graph --decorate=short --color --format=format:'%C(bold blue)%h%C(reset) %C(auto)%d%C(reset)\n         %C(blink yellow)[%cr]%C(reset)  %x09%C(white)%an: %s %C(reset)'"
