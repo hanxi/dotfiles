68b1fdb9ff3c8a8cbe1544d0fc0c08bc5f5e6209
涵曦 <hanxi.info@gmail.com>
2020-06-29 13:48:32 +0800

    Update config.sh

--- etc/config.sh
+++ etc/config.sh
@@ -27,8 +27,8 @@ export TERM=xterm-256color
 export TERM_ITALICS=true
 
 # for http proxy
-export http_proxy=http://192.168.2.5:8080
-#export http_proxy=
+#export http_proxy=http://192.168.2.5:8080
+export http_proxy=
 export https_proxy=$http_proxy
 export ftp_proxy=$http_proxy
 export rsync_proxy=$http_proxy
