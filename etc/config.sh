916675fca5f228c7600bb004fed51b15276cd967
涵曦 <im.hanxi@gmail.com>
2022-07-05 15:29:49 +0900

    set less color

--- etc/config.sh
+++ etc/config.sh
@@ -69,3 +69,20 @@ alias l='ls -CF'
 alias la='ls -A'
 alias ll='ls -alF'
 alias ls='ls --color=auto'
+
+# set less color
+LESS_TERMCAP_mb=$(tput -T ansi blink) # start bold
+LESS_TERMCAP_md=$(tput -T ansi setaf 2 ; tput -T ansi bold) # start bold
+LESS_TERMCAP_me=$(tput -T ansi sgr0)  # turn off bold, blink and underline
+LESS_TERMCAP_so=$(tput -T ansi smso)  # start standout (reverse video)
+LESS_TERMCAP_se=$(tput -T ansi rmso)  # stop standout
+LESS_TERMCAP_us=$(tput -T ansi smul)  # start underline
+LESS_TERMCAP_ue=$(tput -T ansi rmul)  # stop underline
+export LESS_TERMCAP_mb
+export LESS_TERMCAP_md
+export LESS_TERMCAP_me
+export LESS_TERMCAP_se
+export LESS_TERMCAP_so
+export LESS_TERMCAP_ue
+export LESS_TERMCAP_us
+
