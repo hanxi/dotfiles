7fc02c050d447e39f6643013466e7c88981ddf7d
涵曦 <im.hanxi@gmail.com>
2022-10-26 11:38:27 +0800

    update setdocker script

--- bin/setdocker.sh
+++ bin/setdocker.sh
@@ -1,4 +1,9 @@
-sudo groupadd docker
-sudo gpasswd -a ${USER} docker
+sudo docker ps # which works fine
+docker ps # which doesn't work because of permission failure
+sudo addgroup --system docker
+sudo adduser $USER docker
+newgrp docker
+sudo snap restart docker
 sudo service docker restart
-sudo newgrp docker
+sudo chmod 666 /var/run/docker.sock
+docker ps # this now works because my user is in the group
