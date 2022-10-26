f79db5b43ca9976adab4af57d93edf0a09595fab
涵曦 <im.hanxi@gmail.com>
2022-10-26 04:54:44 +0000

    update docker

--- bin/setdocker.sh
+++ bin/setdocker.sh
@@ -1,9 +1,16 @@
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -f --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+
+
 sudo docker ps # which works fine
 docker ps # which doesn't work because of permission failure
 sudo addgroup --system docker
 sudo adduser $USER docker
 newgrp docker
-sudo snap restart docker
-sudo service docker restart
-sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart docker
 docker ps # this now works because my user is in the group
