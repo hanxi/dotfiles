6c3b769d3536ee2d5781e948781ff6f957bc95ce
涵曦 <im.hanxi@gmail.com>
2023-06-07 22:45:01 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,18 +5,13 @@ set -x
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-    CYGWIN*)    machine=Cygwin;;
-    MINGW*)     machine=MinGw;;
-    *)          machine="UNKNOWN:${unameOut}"
+Linux*) machine=Linux ;;
+Darwin*) machine=Mac ;;
+CYGWIN*) machine=Cygwin ;;
+MINGW*) machine=MinGw ;;
+*) machine="UNKNOWN:${unameOut}" ;;
 esac
 echo ${machine}
-
-if [ $machine == "Linux" ]; then
-    echo "install nvim"
-    bash ~/.local/dotfiles/setup_ubuntu2204.sh
-fi
 
 ETC=~/.local/etc
 BIN=~/.local/bin
@@ -26,30 +21,35 @@ mkdir -p $BIN
 # git clone respository
 cd ~/.local/
 if [ -d dotfiles ]; then
-    cd dotfiles
-    git pull
+	cd dotfiles
+	git pull
 else
-    git clone https://github.com/hanxi/dotfiles.git
-    cd dotfiles
+	git clone https://github.com/hanxi/dotfiles.git
+	cd dotfiles
 fi
 cp -rf etc/* $ETC/
 cp -rf bin/* $BIN/
 cp bootstrap.sh $BIN/
 
+if [ $machine == "Linux" ]; then
+	echo "install nvim"
+	bash ~/.local/dotfiles/setup_ubuntu2204.sh
+fi
+
 # source init.sh
 sed -i "\:$ETC/init.sh:d" ~/.bashrc
-echo ". $ETC/init.sh" >> ~/.bashrc
+echo ". $ETC/init.sh" >>~/.bashrc
 . ~/.bashrc
 
 # source vimrc.vim
 touch ~/.vimrc
 sed -i "\:$ETC/vimrc.vim:d" ~/.vimrc
-echo "source $ETC/vimrc.vim" >> ~/.vimrc
+echo "source $ETC/vimrc.vim" >>~/.vimrc
 
 # source tmux.conf
 touch ~/.tmux.conf
 sed -i "\:$ETC/tmux.conf:d" ~/.tmux.conf
-echo "source $ETC/tmux.conf" >> ~/.tmux.conf
+echo "source $ETC/tmux.conf" >>~/.tmux.conf
 
 # update git config
 git config --global color.status auto
@@ -70,9 +70,9 @@ git config --global merge.conflictstyle diff3
 git config --global diff.colorMoved default
 
 # install vim plug
-vim_version=`\vim --version | head -1`
-if [[ `echo $vim_version | awk -F '[ .]' '{print $5}'` -gt 7 ]]; then
-    \vim +PlugInstall +qall
+vim_version=$(\vim --version | head -1)
+if [[ $(echo $vim_version | awk -F '[ .]' '{print $5}') -gt 7 ]]; then
+	\vim +PlugInstall +qall
 fi
 
 # install wezterm
@@ -83,4 +83,3 @@ ln -s $ETC/wezterm ~/.config/wezterm
 # install stylua config
 rm -rf ~/.config/stylua
 ln -s $ETC/stylua ~/.config/stylua
-
