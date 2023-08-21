760303d8e9770f55344af5774ceaec2b18484da6
涵曦 <im.hanxi@gmail.com>
2023-08-21 22:59:29 +0800

    fix proxy

--- etc/config.sh
+++ etc/config.sh
@@ -1,21 +1,20 @@
 # shell prompt
 # from vim ":PromptlineSnapshot ~/.local/etc/shell_prompt.sh airline"
 if [ -f "$HOME/.local/etc/shell_prompt.sh" ]; then
-    . $HOME/.local/etc/shell_prompt.sh
+	. $HOME/.local/etc/shell_prompt.sh
 fi
 
 # enable bash completion
 # yum install bash-completion
-[[ $PS1 && -f /usr/local/share/bash-completion/bash_completion ]] && \
-    . /usr/local/share/bash-completion/bash_completion
-[[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] && \
-    . /usr/share/bash-completion/bash_completion
+[[ $PS1 && -f /usr/local/share/bash-completion/bash_completion ]] &&
+	. /usr/local/share/bash-completion/bash_completion
+[[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] &&
+	. /usr/share/bash-completion/bash_completion
 
 # git completion
 if [ -f /etc/bash_completion.d/git ]; then
-    . /etc/bash_completion.d/git
+	. /etc/bash_completion.d/git
 fi
-
 
 # for .local lib
 export LD_RUN_PATH=$HOME/.local/lib::$LD_RUN_PATH
@@ -28,22 +27,23 @@ export TERM=tmux-256color
 #export TERM_ITALICS=true
 
 # for http proxy
-#export http_proxy=http://192.168.2.5:8080
-export http_proxy=
-export https_proxy=$http_proxy
-export ftp_proxy=$http_proxy
-export rsync_proxy=$http_proxy
-export ALL_PROXY=$http_proxy
+export proxy_addr=
+export HTTPS_PROXY=$proxy_addr
+export http_proxy=$proxy_addr
+export https_proxy=$proxy_addr
+export FTP_PROXY=$proxy_addr
+export RSYNC_PROXY=$proxy_addr
+export ALL_PROXY=$proxy_addr
 
 alias proxyon="setproxy on; . ~/.local/etc/config.sh"
 alias proxyoff="setproxy off; . ~/.local/etc/config.sh"
 
 # editor
-if command -v nvim > /dev/null 2>&1; then
-    alias vim=nvim
-    export VISUAL=nvim
+if command -v nvim >/dev/null 2>&1; then
+	alias vim=nvim
+	export VISUAL=nvim
 else
-    export VISUAL=vim
+	export VISUAL=vim
 fi
 export EDITOR="$VISUAL"
 
@@ -55,19 +55,17 @@ git config --global alias.tree "log --all --graph --decorate=short --color --for
 
 # set pwd for tmux
 function set_tmux_pwd() {
-    [ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD"
-    return 0
+	[ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD"
+	return 0
 }
 function my_cd() {
-    \cd $1
-    set_tmux_pwd
+	\cd $1
+	set_tmux_pwd
 }
 set_tmux_pwd
 alias cd=my_cd
-
 
 alias l='ls -CF'
 alias la='ls -A'
 alias ll='ls -alF'
 alias ls='ls --color=auto'
-
